e7d423ce132776503a47717e8f4d473b02332ce4
Paul Tagliamonte <tag@pault.ag>
2017-11-29 11:27:33 -0500

    Add docs

---
+++ docs.go
@@ -0,0 +1,5 @@
+// Integrity Measurement Architecture, or IMA defines a set of formats for
+// signing and measuring files. This package contains a set of parsers to read
+// and validate IMA Signatures, and interfaces to allow crypto.Signer to create
+// new Signatures over new or existing files.
+package ima
---
+++ xattr/docs.go
@@ -0,0 +1,3 @@
+// Bindings to read, write and validate IMA signatures stored as an xattr
+// on a file.
+package xattr
